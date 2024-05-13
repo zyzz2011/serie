a849e7f6f021fb62c214fd387d8733aece424c54
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-05-13 15:19:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Narcos");
+     System.out.println("é uma série de televisão que mergulha nas complexidades do mundo do tráfico de drogas na Colômbia e além. A trama segue a ascensão do infame traficante Pablo Escobar, desde seus humildes começos até se tornar um dos homens mais ricos e poderosos do mundo, enquanto ele constrói seu império do narcotráfico. Ao longo das temporadas, a série aborda não apenas os aspectos da vida de Escobar, mas também a luta das autoridades colombianas e americanas para capturá-lo e o subsequente vazio de poder deixado por sua queda. Com uma mistura de drama, ação e intriga política, Narcos oferece uma visão fascinante e muitas vezes perturbadora do submundo do crime organizado");
+  }
+  
+
+  
+
+  
+}
